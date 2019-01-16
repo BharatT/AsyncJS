493c277fedcf4d3f26332a96e1ded776a28241a3
Bharat <btiwari033@gmail.com>
2019-01-17 01:59:04 +0530

    Promise.all use for multipal promise

--- promises.js
+++ promises.js
@@ -27,11 +27,21 @@ function createPost(post){
     });
     
 }
-
+/*
 createPost({title: "Post three", Body : " This is post three"})
             .then(getPosts)
             .catch(err => console.log(err));
+*/
 
+// promice.all
+
+
+const promic1 = Promise.resolve("Hello Bharat");
+const promic2 = 10;
+const promic3 = new Promise((resolve, reject) => setTimeout(resolve,2000, ' Jai Shree Ram'));
+
+
+Promise.all([promic1,promic2,promic3]).then(value => console.log(value));
 
 
 
